b1ff47f3ede78daa6583576893f8fa48efd5fef5
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-14 20:43:45 -0600

    Update Ejercicio_9.cs

--- Ejercicio_9.cs
+++ Ejercicio_9.cs
@@ -6,3 +6,31 @@ Dado dos números, muestra el número mayor
 utilizando una estructura if
 
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, introduce el primer numero: ");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Por favor, introduce el segundo numero: ");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+
+        if (numero1 > numero2)
+        {
+            Console.WriteLine("El numero mayor es: " + numero1);
+        }
+        else if (numero2 > numero1)
+        {
+            Console.WriteLine("El numero mayor es: " + numero2);
+        }
+        else
+        {
+            Console.WriteLine("Los numeros son iguales.");
+        }
+    }
+}
+
