254b5e0bf0b23aa0995e4fde96f78cec34f4f3b8
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-14 20:41:20 -0600

    Update Ejercicio_8.cs

--- Ejercicio_8.cs
+++ Ejercicio_8.cs
@@ -20,3 +20,20 @@ la siguiente secuencia de patrones:
 *****
 
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
