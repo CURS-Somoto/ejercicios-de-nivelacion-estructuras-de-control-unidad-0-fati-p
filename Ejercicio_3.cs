b47b8a6ed6c8ccc5dd0d9cff6091e65c9e1a74d2
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-14 20:28:58 -0600

    Update Ejercicio_3.cs
    ejercicio 4

--- Ejercicio_3.cs
+++ Ejercicio_3.cs
@@ -3,3 +3,41 @@
 
 Escribe un
 programa que determine si un número dado es primo o no.*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, introduce un numero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        bool esPrimo = true;
+
+        if (numero == 1)
+        {
+            esPrimo = false;
+        }
+        else
+        {
+            for (int i = 2; i <= numero / 2; i++)
+            {
+                if (numero % i == 0)
+                {
+                    esPrimo = false;
+                    break;
+                }
+            }
+        }
+
+        if (esPrimo)
+        {
+            Console.WriteLine("El numero es primo.");
+        }
+        else
+        {
+            Console.WriteLine("El numero no es primo.");
+        }
+    }
+}
