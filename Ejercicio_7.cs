9bdca8012063ccbd2d3f05b358e87b30f0e91fb4
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-14 20:39:40 -0600

    Update Ejercicio_7.cs

--- Ejercicio_7.cs
+++ Ejercicio_7.cs
@@ -4,3 +4,17 @@
 Imprime un conteo regresivo del 10 al 1
 utilizando un bucle for.
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 10; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
