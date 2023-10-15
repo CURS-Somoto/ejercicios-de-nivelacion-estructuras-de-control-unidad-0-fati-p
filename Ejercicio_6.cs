16e268d09946fb1a2ee2356f592d14770eaff7b2
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-14 20:36:20 -0600

    Update Ejercicio_6.cs

--- Ejercicio_6.cs
+++ Ejercicio_6.cs
@@ -3,3 +3,23 @@
 
 Escribe un programa que diga si un número
 es par o impar.*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, introduce un numero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El numero es par.");
+        }
+        else
+        {
+            Console.WriteLine("El numero es impar.");
+        }
+    }
+}
