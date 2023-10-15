a95b96fab5e8a8511ac2fb6c031549a62554c570
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-14 20:31:48 -0600

    Update Ejercicio_4.cs
    ejercicio 5

--- Ejercicio_4.cs
+++ Ejercicio_4.cs
@@ -3,3 +3,17 @@
 
 Crea un programa que genere y muestre el
 resultado factorial de un numero. */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero = Convert.ToInt32(Console.ReadLine());
+        long factorial = 1;
+        for (int i = 2; i <= numero; i++)
+            factorial *= i;
+        Console.WriteLine(factorial);
+    }
+}
