2653e8edacedad592ebde9b954baac439ff97b71
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-14 20:16:47 -0600

    Update Ejercicio_1.cs
    ejercicio 1

--- Ejercicio_1.cs
+++ Ejercicio_1.cs
@@ -1,5 +1,20 @@
 /*Ejercicio 1: Impresión de Números Pares
 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 2; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
 
 Escribe un programa
 que muestre los números pares del 2 al 20. */
