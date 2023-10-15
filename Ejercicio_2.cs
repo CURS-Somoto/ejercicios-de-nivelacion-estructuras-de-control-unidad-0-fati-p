ea3d707a471d8828789b4e30f718ece133013938
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-14 20:26:22 -0600

    Update Ejercicio_2.cs
    ejercicio 3

--- Ejercicio_2.cs
+++ Ejercicio_2.cs
@@ -3,3 +3,21 @@
 
 Crea un programa que calcule y muestre la
 suma de los dígitos de un número ingresado por el usuario.*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, introduce el primer numero: ");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Por favor, introduce el segundo numero: ");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+
+        int suma = numero1 + numero2;
+
+        Console.WriteLine("La suma de los dos numeros es: " + suma);
+    }
+}
